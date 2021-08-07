36b6279f6f576ad48eccc7d2732c09c5d6cc2d04
qayserily <muratbayram2005@gmail.com>
2021-08-07 18:58:32 +0300

    step 2

--- src/test/java/com/vytrack/runner/CukesRunner.java
+++ src/test/java/com/vytrack/runner/CukesRunner.java
@@ -6,15 +6,17 @@ import org.junit.runner.RunWith;
 
 //specifying to run it with cucumber
 @RunWith(Cucumber.class)
-/*providing option for cucumber to provide
- * where is your feature file    |  features = "feature file folder path here"
- * where is  the code that match the steps in cucumber | glue = "step defitions folder path here"
- * wether to run the actual code or just to check if you have missing steps | dryRun = true/false
- * what kind of report you want to get and other options | plugins= ...
+/*
+ providing option for cucumber to provide
+ * where is your feature file     |  features = "feature file folder path here"
+ * where is the code that match the steps in cucumber  | glue = " step definitions folder path here"
+ * whether to run the actual code or just to check if you have missing steps  | dryRun = true/false
+ * what kind of report you want to get and other options | plugins = ....
  */
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "src/test/java/com/vytrack/step_definitions"
-)
+
+@CucumberOptions(features = "src/test/resources/features" ,
+        glue = "com/vytrack/step_definitions")
+
+
 public class CukesRunner {
 }
--- src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
@@ -5,20 +5,24 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CalculatorStepDefs {
+
     @Given("I have calculator app open")
-    public void ı_have_calculator_app_open() {
+    public void i_have_calculator_app_open() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Running step : I have calculator app open");
     }
 
     @When("I add {int} to {int}")
-    public void ı_add_to(Integer int1, Integer int2) {
+    public void i_add_to(Integer num1, Integer num2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Running step : I add " + num1 + " to " + num2);
     }
+
     @Then("I should get {int}")
-    public void ı_should_get(Integer int1) {
+    public void i_should_get(Integer expected) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Running step : Verifying the sum should be " + expected);
     }
+
+
 }
